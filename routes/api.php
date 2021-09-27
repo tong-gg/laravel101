144c032511ddc8aaf773d5026ed529a2b0b786be
Patipan <patipan.bo@ku.th>
2021-09-27 13:04:44 +0700

    add API routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// using api middleware
+Route::apiResource('apartments', \App\Http\Controllers\Api\ApartmentController::class);
